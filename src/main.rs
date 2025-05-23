5226eaf4c4a256ce6d3b83546fbecda94974a47a
David Bernard <david.bernard.31@gmail.com>
2025-05-23 16:18:56 +0200

    chore: init the project (2)

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,21 @@
 pub fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    #[test]
+    fn test_sample() {
+        insta::assert_snapshot!(indoc! {r#"
+                {
+                    "a": "Hello",
+                    "id": 123456,
+                    "details": {
+                        "user": "johnD",
+                        "name": "John Doe",
+                        "url": "http://example.com/item/123456"
+                    }
+                }
+            "#},);
+    }
+}
